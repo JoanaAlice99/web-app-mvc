76839179709a883f375d4b42e8c265c49d0d97e5
JuTheCutie <joelalexandreff@hotmail.com>
2020-10-27 19:40:35 +0000

    update

--- web-app-mvc/Controllers/ArtigoController.cs
+++ web-app-mvc/Controllers/ArtigoController.cs
@@ -3,13 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using MySqlConnector;
 
 namespace web_app_mvc.Controllers
 {
     public class ArtigoController : Controller
     {
+
+        private readonly IConfiguration configuration;
+
         public IActionResult Index()
         {
+            string connectionString = configuration.GetConnectionString("Def");
+            var connection = new MySqlConnection(connectionString);
+
+            connection.Open();
+
+            using var command = new MySqlCommand("SELECT * FROM Artigos;", connection);
+            using var reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                var value = reader.GetValue(0);
+                ViewData["data"] = value;
+            }
+
+
             return View();
         }
     }
