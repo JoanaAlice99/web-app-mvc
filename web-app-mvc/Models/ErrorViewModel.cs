0471de379b0eef3f6ca6c537cbb65ab9c51588ae
JuTheCutie <joelalexandreff@hotmail.com>
2020-10-27 17:06:35 +0000

    update

---
+++ web-app-mvc/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace web_app_mvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
